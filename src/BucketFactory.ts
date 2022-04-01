8af7c905369accdcb62d558402598ba6205471a6
rachel <rachel.fu@newfront.com>
2022-04-01 16:36:17 -0700

    Clean up all Round related logic and update all handlers

--- src/BucketFactory.ts
+++ src/BucketFactory.ts
@@ -1,40 +1,35 @@
 
-import { Bucket, BucketRound } from '../generated/schema'
-import { BigInt, log } from '@graphprotocol/graph-ts'
+import { Bucket } from '../generated/schema'
+import { log } from '@graphprotocol/graph-ts'
 import { Bucket as I_Bucket } from '../generated/templates'
 import { Create as E_Create } from '../generated/templates/BucketFactory/BucketFactory'
 
 
 //event Create(address creator, address bucket, uint256 startEpoch, uint256 round, uint256 numPerRound, string uri);
 export function handleCreate(event: E_Create): void {
+    log.info("createnewbucket:{}",[event.params.bucket.toHex()])
     let bucket = new Bucket(event.params.bucket.toHex())
     bucket.state = "pending"
     bucket.txHash = event.transaction.hash
     bucket.creator = event.params.creator.toHex()
     bucket.uri = event.params.uri
-    bucket.name = "xx"
-    bucket.description = "xxx"
-    bucket.discord = "xxxx"
-    bucket.twitter = "xxxxx"
-    bucket.website = "xxxxxx"
-    bucket.whitelistPerRound = event.params.numPerRound.toI32()
-    bucket.numRounds = event.params.round.toI32()
+    bucket.name = "bucket name"
+    bucket.description = "bucket description"
+    bucket.discord = "discord"
+    bucket.twitter = "twitter"
+    bucket.website = "website"
+    bucket.whitelistAmount = event.params.numPerRound.toI32()
     bucket.mintPrice = 0
-    bucket.mintToken = "weth"
+    bucket.mintToken = "mint token"
     bucket.blockchain = "ethereum"
     bucket.launchDate = 0
     bucket.createTime = event.block.timestamp.toI32()
+    bucket.startEpoch = "startEpoch"
+    bucket.endEpoch = "endEpoch"
+    bucket.ticketsAmountInBucket = 0
+    bucket.winners = []
 
 
-    for(let i = 0; i < event.params.round.toI32(); i++) {
-        let round = new BucketRound(event.params.bucket.toHex()+i.toString())
-        round.round = i
-        round.bucket = bucket.id
-        round.state = "pending"
-        round.tickets = event.params.numPerRound.toI32()
-        round.save()
-        // bucket.rounds.push(round.id) no need to add this because Bucketround field in Bucket can be derived
-    }
     bucket.save()
     I_Bucket.create(event.params.bucket)
     log.info("create new bucket:{}",[event.params.bucket.toHex()])
