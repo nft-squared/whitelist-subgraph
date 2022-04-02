d91b999dab6c94776cd755df4ddbbc2e18609011
rachel <rachel.fu@newfront.com>
2022-04-01 17:06:32 -0700

    Handle weight

---
+++ src/Weights.ts
@@ -0,0 +1,22 @@
+import { Bucket,UserBucketTicket, AuthedToken, User, TokenWeight } from '../generated/schema'
+import { log } from '@graphprotocol/graph-ts'
+import { UpdateWeights as E_UpdateWeights } from '../generated/templates/Weights/Weights'
+
+// event UpdateWeights(uint256 optEpoch, uint256 epoch, address[] tokens, uint256[] weights);
+export function handleUpdateWeights(event: E_UpdateWeights): void {
+    let epochId = event.params.epoch.toHex()
+    let tokens = event.params.tokens
+    let weights = event.params.weights
+    for (let i = 0; i < tokens.length; i++) {
+        let tokenAddress = tokens[i].toHex()
+        let tokenWeightId = epochId + tokenAddress
+        let tokenWeight = TokenWeight.load(tokenWeightId)
+        if (tokenWeight === null) {
+            tokenWeight = new TokenWeight(tokenWeightId)
+        }
+        tokenWeight.epoch = epochId
+        tokenWeight.tokenAddress = tokenAddress
+        tokenWeight.weight = weights[i].toI32()
+        tokenWeight.save()
+    }
+}
