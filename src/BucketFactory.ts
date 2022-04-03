3920118f2e2244a06d67c017d3acf20f307b3070
rachel <rachel.fu@newfront.com>
2022-04-03 09:26:19 -0700

    Add epoch

--- src/BucketFactory.ts
+++ src/BucketFactory.ts
@@ -8,6 +8,8 @@ import { Create as E_Create } from '../generated/templates/BucketFactory/BucketF
 //event Create(address creator, address bucket, uint256 startEpoch, uint256 round, uint256 numPerRound, string uri);
 export function handleCreate(event: E_Create): void {
     log.info("createnewbucket:{}",[event.params.bucket.toHex()])
+
+
     let bucket = new Bucket(event.params.bucket.toHex())
     bucket.state = "pending"
     bucket.txHash = event.transaction.hash
@@ -24,13 +26,14 @@ export function handleCreate(event: E_Create): void {
     bucket.blockchain = "ethereum"
     bucket.launchDate = 0
     bucket.createTime = event.block.timestamp.toI32()
-    bucket.startEpoch = "startEpoch"
-    bucket.endEpoch = "endEpoch"
+    bucket.startEpoch = event.params.startEpoch.toString()
+    bucket.endEpoch = "10"
     bucket.ticketsAmountInBucket = 0
     bucket.winners = []
 
 
     bucket.save()
+
     I_Bucket.create(event.params.bucket)
     log.info("create new bucket:{}",[event.params.bucket.toHex()])
 }
